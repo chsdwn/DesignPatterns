80d43bfe1fbe1c419f9bfdea7abc84990acf01a1
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-09-30 17:54:53 +0300

    4. Builder

--- Program.cs
+++ Program.cs
@@ -9,6 +9,76 @@ using static System.Console;
 
 namespace DesignPatterns
 {
+    public class HtmlElement
+    {
+        private const int INDENT_SIZE = 2;
+
+        public string Name, Text;
+        public List<HtmlElement> Elements = new List<HtmlElement>();
+
+        public HtmlElement()
+        {
+        }
+
+        public HtmlElement(string name, string text)
+        {
+            Name = name;
+            Text = text;
+        }
+
+        private string ToStringImpl(int indent)
+        {
+            var stringBuilder = new StringBuilder();
+            var i = new string(' ', INDENT_SIZE * indent);
+            stringBuilder.AppendLine($"{i}<{Name}>");
+
+            if (!string.IsNullOrWhiteSpace(Text))
+            {
+                stringBuilder.Append(new string(' ', INDENT_SIZE * (indent + 1)));
+                stringBuilder.AppendLine(Text);
+            }
+
+            foreach (var element in Elements)
+                stringBuilder.Append(element.ToStringImpl(indent + 1));
+            stringBuilder.AppendLine($"{i}</{Name}>");
+
+            return stringBuilder.ToString();
+        }
+
+        public override string ToString()
+        {
+            return ToStringImpl(0);
+        }
+    }
+
+    public class HtmlBuilder
+    {
+        private readonly string _rootName;
+        HtmlElement root = new HtmlElement();
+
+        public HtmlBuilder(string rootName)
+        {
+            _rootName = rootName;
+            root.Name = rootName;
+        }
+
+        public void AddChild(string childName, string childText)
+        {
+            var element = new HtmlElement(childName, childText);
+            root.Elements.Add(element);
+        }
+
+        public override string ToString()
+        {
+            return root.ToString();
+        }
+
+        public void Clear()
+        {
+            root = new HtmlElement { Name = _rootName };
+        }
+    }
+
     public class Program
     {
         static void Main(string[] args)
@@ -27,6 +97,11 @@ namespace DesignPatterns
                 stringBuilder.AppendFormat("<li>{0}</li>", word);
             stringBuilder.Append("</ul>");
             WriteLine(stringBuilder);
+
+            var htmlBuilder = new HtmlBuilder("ul");
+            htmlBuilder.AddChild("li", "hello");
+            htmlBuilder.AddChild("li", "world");
+            WriteLine(htmlBuilder.ToString());
         }
     }
 }
