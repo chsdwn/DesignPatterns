1341be65c7732f84f315ce0f63d92383d9a99b70
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-09-30 15:46:36 +0300

    2. Single Responsibility Principle

--- Program.cs
+++ Program.cs
@@ -1,12 +1,54 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using static System.Console;
 
 namespace DesignPatterns
 {
+    public class Journal
+    {
+        private readonly List<string> _entries = new List<string>();
+        private static int _count = 0;
+
+        public int AddEntry(string text)
+        {
+            _entries.Add($"{++_count}: {text}");
+            return _count; // memento pattern
+        }
+
+        public void RemoveEntry(int index)
+        {
+            _entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return String.Join(Environment.NewLine, _entries);
+        }
+    }
+
+    public class Persistence
+    {
+        public void SaveToFile(Journal journal, string filename, bool overwrite = false)
+        {
+            if (overwrite || !File.Exists(filename))
+                File.WriteAllText(filename, journal.ToString());
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var journal = new Journal();
+            journal.AddEntry("I cried today");
+            journal.AddEntry("I ate a bug");
+            WriteLine(journal);
+
+            var persistence = new Persistence();
+            var filename = @".\journal.txt";
+            persistence.SaveToFile(journal, filename, true);
         }
     }
 }
