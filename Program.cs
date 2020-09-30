686f8682ee8707fe1e0bb75073049d571e747f0b
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-09-30 16:33:36 +0300

    3. Open-Closed Principle

--- Program.cs
+++ Program.cs
@@ -1,54 +1,147 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Drawing;
 using System.IO;
 using static System.Console;
 
 namespace DesignPatterns
 {
-    public class Journal
+    public enum Name
     {
-        private readonly List<string> _entries = new List<string>();
-        private static int _count = 0;
+        Red, Green, Blue
+    }
 
-        public int AddEntry(string text)
-        {
-            _entries.Add($"{++_count}: {text}");
-            return _count; // memento pattern
-        }
+    public enum Size
+    {
+        Small, Medium, Large, Huge
+    }
 
-        public void RemoveEntry(int index)
-        {
-            _entries.RemoveAt(index);
-        }
+    public class Product
+    {
+        public string Name { get; set; }
+        public Color Color { get; set; }
+        public Size Size { get; set; }
 
-        public override string ToString()
+        public Product(string name, Color color, Size size)
         {
-            return String.Join(Environment.NewLine, _entries);
+            if (name == null)
+                throw new ArgumentNullException(paramName: nameof(name));
+
+            Name = name;
+            Color = color;
+            Size = size;
         }
     }
 
-    public class Persistence
+    public class ProductFilter
     {
-        public void SaveToFile(Journal journal, string filename, bool overwrite = false)
+        public static IEnumerable<Product> FilterBySize(IEnumerable<Product> products, Size size)
         {
-            if (overwrite || !File.Exists(filename))
-                File.WriteAllText(filename, journal.ToString());
+            foreach (var product in products)
+                if (product.Size == size)
+                    yield return product;
+        }
+        public static IEnumerable<Product> FilterByColor(IEnumerable<Product> products, Color color)
+        {
+            foreach (var product in products)
+                if (product.Color == color)
+                    yield return product;
         }
     }
 
-    class Program
+    public interface ISpecification<T>
+    {
+        bool IsSatisfied(T t);
+    }
+
+    public interface IFilter<T>
+    {
+        IEnumerable<T> Filter(IEnumerable<T> items, ISpecification<T> spec);
+    }
+
+    public class ColorSpecification : ISpecification<Product>
+    {
+        private Color _color;
+
+        public ColorSpecification(Color color)
+        {
+            _color = color;
+        }
+
+        public bool IsSatisfied(Product product)
+        {
+            return product.Color == _color;
+        }
+    }
+    public class SizeSpecification : ISpecification<Product>
+    {
+        private Size _size;
+
+        public SizeSpecification(Size size)
+        {
+            _size = size;
+        }
+
+        public bool IsSatisfied(Product product)
+        {
+            return product.Size == _size;
+        }
+    }
+
+    public class AndSpecification<T> : ISpecification<T>
+    {
+        private ISpecification<T> _first, _second;
+
+        public AndSpecification(ISpecification<T> first, ISpecification<T> second)
+        {
+            _first = first;
+            _second = second;
+        }
+
+        public bool IsSatisfied(T t)
+        {
+            return _first.IsSatisfied(t) && _second.IsSatisfied(t);
+        }
+    }
+
+    public class BetterFilter : IFilter<Product>
+    {
+        public IEnumerable<Product> Filter(IEnumerable<Product> items, ISpecification<Product> spec)
+        {
+            foreach (var product in items)
+                if (spec.IsSatisfied(product))
+                    yield return product;
+        }
+    }
+
+    public class Program
     {
         static void Main(string[] args)
         {
-            var journal = new Journal();
-            journal.AddEntry("I cried today");
-            journal.AddEntry("I ate a bug");
-            WriteLine(journal);
+            var apple = new Product("Apple", Color.Green, Size.Small);
+            var tree = new Product("Tree", Color.Green, Size.Large);
+            var house = new Product("House", Color.Blue, Size.Large);
 
-            var persistence = new Persistence();
-            var filename = @".\journal.txt";
-            persistence.SaveToFile(journal, filename, true);
+            Product[] products = { apple, tree, house };
+
+            WriteLine("Green products (old): ");
+            foreach (var product in ProductFilter.FilterByColor(products, Color.Green))
+                WriteLine($" - {product.Name} is green");
+
+            var betterFilter = new BetterFilter();
+            WriteLine("Green products (new): ");
+            foreach (var product in betterFilter.Filter(products, new ColorSpecification(Color.Green)))
+                WriteLine($" - {product.Name} is green");
+
+            WriteLine("Large blue items: ");
+            foreach (var product in betterFilter.Filter(
+                products,
+                new AndSpecification<Product>(
+                    new ColorSpecification(Color.Blue),
+                    new SizeSpecification(Size.Large))
+                ))
+                WriteLine($" - {product.Name} is large an blue");
         }
     }
 }
