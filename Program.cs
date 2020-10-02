581117739a83745d8567e33705d7336c8d8546c4
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-10-02 11:41:47 +0300

    6. Fluent Builder Inheritance with Recursive Generics

--- Program.cs
+++ Program.cs
@@ -9,74 +9,49 @@ using static System.Console;
 
 namespace DesignPatterns
 {
-    public class HtmlElement
+    public class Person
     {
-        private const int INDENT_SIZE = 2;
+        public string Name;
+        public string Position;
 
-        public string Name, Text;
-        public List<HtmlElement> Elements = new List<HtmlElement>();
-
-        public HtmlElement()
+        public class Builder : PersonJobBuilder<Builder>
         {
         }
 
-        public HtmlElement(string name, string text)
-        {
-            Name = name;
-            Text = text;
-        }
-
-        private string ToStringImpl(int indent)
-        {
-            var stringBuilder = new StringBuilder();
-            var i = new string(' ', INDENT_SIZE * indent);
-            stringBuilder.AppendLine($"{i}<{Name}>");
-
-            if (!string.IsNullOrWhiteSpace(Text))
-            {
-                stringBuilder.Append(new string(' ', INDENT_SIZE * (indent + 1)));
-                stringBuilder.AppendLine(Text);
-            }
-
-            foreach (var element in Elements)
-                stringBuilder.Append(element.ToStringImpl(indent + 1));
-            stringBuilder.AppendLine($"{i}</{Name}>");
-
-            return stringBuilder.ToString();
-        }
+        public static Builder New => new Builder();
 
         public override string ToString()
         {
-            return ToStringImpl(0);
+            return $"{nameof(Name)}: {Name}, {nameof(Position)}: {Position}";
         }
     }
 
-    public class HtmlBuilder
+    public abstract class PersonBuilder
     {
-        private readonly string _rootName;
-        HtmlElement root = new HtmlElement();
+        protected Person _person = new Person();
 
-        public HtmlBuilder(string rootName)
+        public Person Build() => _person;
+    }
+
+    public class PersonInfoBuilder<SELF>
+        : PersonBuilder
+        where SELF : PersonInfoBuilder<SELF>
+    {
+        public SELF Called(string name)
         {
-            _rootName = rootName;
-            root.Name = rootName;
+            _person.Name = name;
+            return (SELF)this;
         }
+    }
 
-        public HtmlBuilder AddChild(string childName, string childText)
+    public class PersonJobBuilder<SELF>
+        : PersonInfoBuilder<PersonJobBuilder<SELF>>
+        where SELF : PersonJobBuilder<SELF>
+    {
+        public SELF WorksAsA(string position)
         {
-            var element = new HtmlElement(childName, childText);
-            root.Elements.Add(element);
-            return this;
-        }
-
-        public override string ToString()
-        {
-            return root.ToString();
-        }
-
-        public void Clear()
-        {
-            root = new HtmlElement { Name = _rootName };
+            _person.Position = position;
+            return (SELF)this;
         }
     }
 
@@ -84,26 +59,11 @@ namespace DesignPatterns
     {
         static void Main(string[] args)
         {
-            var hello = "hello";
-            var stringBuilder = new StringBuilder();
-            stringBuilder.Append("<p>");
-            stringBuilder.Append(hello);
-            stringBuilder.Append("</p>");
-            WriteLine(stringBuilder);
-
-            var words = new[] { "hello", "world" };
-            stringBuilder.Clear();
-            stringBuilder.Append("<ul>");
-            foreach (var word in words)
-                stringBuilder.AppendFormat("<li>{0}</li>", word);
-            stringBuilder.Append("</ul>");
-            WriteLine(stringBuilder);
-
-            var htmlBuilder = new HtmlBuilder("ul");
-            htmlBuilder
-                .AddChild("li", "hello")
-                .AddChild("li", "world");
-            WriteLine(htmlBuilder.ToString());
+            var person = Person.New
+                .Called("Hulusi")
+                .WorksAsA("Developer")
+                .Build();
+            WriteLine(person);
         }
     }
 }
