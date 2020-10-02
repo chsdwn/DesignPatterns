ea33cbde3b1885a3977319141196a37de878ce3c
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-10-02 18:51:22 +0300

    5. Inner Factory

--- Program.cs
+++ Program.cs
@@ -9,30 +9,14 @@ using static System.Console;
 
 namespace DesignPatterns
 {
-    public enum CoordinateSystem
-    {
-        Cartesian,
-        Polar
-    }
-
-    public static class PointFactory
-    {
-        public static Point NewCarteshianPoint(double x, double y)
-        {
-            return new Point(x, y);
-        }
-
-        public static Point NewPolarPoint(double rho, double theta)
-        {
-            return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
-        }
-    }
-
     public class Point
     {
+        // public static Point Origin => new Point(0, 0);
+        public static Point Origin = new Point(0, 0); // better
+
         private double _x, _y;
 
-        public Point(double x, double y)
+        private Point(double x, double y)
         {
             _x = x;
             _y = y;
@@ -42,14 +26,30 @@ namespace DesignPatterns
         {
             return $"{nameof(_x)}: {_x}, {nameof(_y)} {_y}";
         }
+
+        public static class Factory
+        {
+            public static Point NewCarteshianPoint(double x, double y)
+            {
+                return new Point(x, y);
+            }
+
+            public static Point NewPolarPoint(double rho, double theta)
+            {
+                return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+            }
+        }
     }
 
     public class Program
     {
         static void Main(string[] args)
         {
-            var point = PointFactory.NewPolarPoint(1.0, Math.PI / 2);
+            var point = Point.Factory.NewPolarPoint(1.0, Math.PI / 2);
             WriteLine(point);
+
+            var origin = Point.Origin;
+            WriteLine(origin);
         }
     }
 }
