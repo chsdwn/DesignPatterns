7e413048d586f4897c5b5201a0956500e93525a8
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-10-05 11:45:59 +0300

    2. Singleton Implementation

--- Program.cs
+++ Program.cs
@@ -7,92 +7,50 @@ using System.Linq;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Xml.Serialization;
+using MoreLinq;
 using static System.Console;
 
 namespace DesignPatterns
 {
-    public static class ExtensionMethods
+    public interface IDatabase
     {
-        public static T DeepCopy<T>(this T self)
-        {
-            var stream = new MemoryStream();
-            var formatter = new BinaryFormatter();
-            formatter.Serialize(stream, self);
-            stream.Seek(0, SeekOrigin.Begin);
-            object copy = formatter.Deserialize(stream);
-            stream.Close();
-            return (T)copy;
-        }
-
-        public static T DeepCopyXml<T>(this T self)
-        {
-            using (var stream = new MemoryStream())
-            {
-                var xmlSerializer = new XmlSerializer(typeof(T));
-                xmlSerializer.Serialize(stream, self);
-                stream.Position = 0;
-                return (T)xmlSerializer.Deserialize(stream);
-            }
-        }
+        int GetPopulation(string name);
     }
 
-    // needed for DeepCopy()
-    [Serializable]
-    public class Person
+    public class SingletonDatabase : IDatabase
     {
-        public string[] Names;
-        public Address Address;
+        private Dictionary<string, int> _capitals;
 
-        // needed for DeepCopyXml()
-        public Person()
+        private SingletonDatabase()
         {
+            WriteLine("Initializing database...");
+
+            _capitals = File.ReadAllLines("capitals.txt")
+               .Batch(2)
+               .ToDictionary(
+                   list => list.ElementAt(0).Trim(),
+                   list => int.Parse(list.ElementAt(1))
+               );
         }
 
-        public Person(string[] names, Address address)
+        public int GetPopulation(string name)
         {
-            Names = names;
-            Address = address;
+            return _capitals[name];
         }
 
-        public override string ToString()
-        {
-            return $"{nameof(Names)}: {string.Join(" ", Names)}, {nameof(Address)}: {Address}";
-        }
-    }
+        private static Lazy<SingletonDatabase> _instance =
+            new Lazy<SingletonDatabase>(() => new SingletonDatabase());
 
-    [Serializable]
-    public class Address
-    {
-        public string StreetName;
-        public int HouseNumber;
-
-        public Address()
-        {
-        }
-
-        public Address(string streetName, int houseNumber)
-        {
-            StreetName = streetName;
-            HouseNumber = houseNumber;
-        }
-
-        public override string ToString()
-        {
-            return $"{nameof(StreetName)}: {StreetName}, {nameof(HouseNumber)}: {HouseNumber}";
-        }
+        public static SingletonDatabase Instance => _instance.Value;
     }
 
     public class Program
     {
         static void Main(string[] args)
         {
-            var ali = new Person(new[] { "Ali", "Veli" }, new Address("A Street", 100));
-            var ayse = ali.DeepCopy();
-            ayse.Names[0] = "Ayse";
-            ayse.Address.HouseNumber = 200;
-
-            WriteLine(ali);
-            WriteLine(ayse);
+            var db = SingletonDatabase.Instance;
+            var city = "Tokyo";
+            WriteLine($"{city} has population {db.GetPopulation(city)}");
         }
     }
 }
